3a382b7808ed3b777e876433f2ff5d89ab4a2b56
Bennett Potgieter <potgieterbt@gmail.com>
2024-03-12 10:35:14 +0000

    sdl testing stuff

--- src/test.zig
+++ src/test.zig
@@ -1,8 +1,14 @@
 const std = @import("std");
 const fs = std.fs;
 const print = std.debug.print;
-const sdl = @cImport({
+const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
 
-pub fn main() !void {}
+pub fn main() !void {
+    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
+        c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    }
+    defer c.SDL_Quit();
+}
